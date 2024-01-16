d9731331fc49cd99aed919c14f0ebe88cd736dd1
medherradi <med.herradi@gmail.com>
2024-01-16 15:58:11 +0100

    adding func related struct

--- app.go
+++ app.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"revenue.com/project/prices"
 )
 
 func main() {
-	prices := []float64{10, 20, 30}
+
 	taxRates := []float64{0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-	fmt.Println(result)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for idx, val := range prices {
-
-			taxIncludedPrices[idx] = val * (1 + taxRate)
-
-			result[taxRate] = taxIncludedPrices
-		}
-
+		priceJob := prices.NewTaxIncludPrice(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,33 @@
 package prices
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type TaxIncludedPrice struct {
 	TaxRate         float64
 	InPrices        []float64
 	TaxIncludPrices map[string][]float64
+}
+
+// function related struct
+func (p TaxIncludedPrice) Process() {
+	result := make(map[string]float64)
+
+	for _, val := range p.InPrices {
+
+		result[strconv.Itoa(int(val))] = val * (1 + p.TaxRate)
+
+	}
+	fmt.Println(result)
+}
+
+// adding constructor function with New key word
+
+func NewTaxIncludPrice(taxRate float64) *TaxIncludedPrice {
+	return &TaxIncludedPrice{
+		TaxRate:  taxRate,
+		InPrices: []float64{10, 20, 30},
+	}
 }
